4ca9beeebec2b2076588928115e2ca5b0a415401
Edward Alexis <edalvb@gmail.com>
2022-01-19 14:32:20 -0500

    #24 Diseño FilterPage #2

---
+++ lib/src/features/presentation/filter_page/custom_widgets/cuisines_filter.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:ytb_flutter_delivery_firebase/src/colors/colors.dart';
+
+class CuisinesFilter extends StatefulWidget {
+  CuisinesFilter({Key? key}) : super(key: key);
+
+  @override
+  _CuisinesFilterState createState() => _CuisinesFilterState();
+}
+
+class _CuisinesFilterState extends State<CuisinesFilter> {
+  bool btnAmerica = false;
+  bool btnSushi = false;
+  bool btnAsia = false;
+  bool btnPizza = false;
+
+  bool btnDesserts = false;
+  bool btnFastFood = false;
+  bool btnVietnamese = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _roudendButtonFilter(
+              labelText: 'America',
+              isActive: btnAmerica,
+              onPressed: () => setState(() => btnAmerica = !btnAmerica),
+            ),
+            _roudendButtonFilter(
+              labelText: 'Asia',
+              isActive: btnAsia,
+              onPressed: () => setState(() => btnAsia = !btnAsia),
+            ),
+            _roudendButtonFilter(
+              labelText: 'Shushi',
+              isActive: btnSushi,
+              onPressed: () => setState(() => btnSushi = !btnSushi),
+            ),
+            _roudendButtonFilter(
+              labelText: 'Pizza',
+              isActive: btnPizza,
+              onPressed: () => setState(() => btnPizza = !btnPizza),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _roudendButtonFilter(
+              labelText: 'Desserts',
+              isActive: btnDesserts,
+              onPressed: () => setState(() => btnDesserts = !btnDesserts),
+            ),
+            _roudendButtonFilter(
+              labelText: 'Fast Food',
+              isActive: btnFastFood,
+              onPressed: () => setState(() => btnFastFood = !btnFastFood),
+            ),
+            _roudendButtonFilter(
+              labelText: 'Vietnamese',
+              isActive: btnVietnamese,
+              onPressed: () => setState(() => btnVietnamese = !btnVietnamese),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+Widget _roudendButtonFilter({
+  required bool isActive,
+  required String labelText,
+  void Function()? onPressed,
+}) {
+  return ElevatedButton(
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      elevation: 0.5,
+      primary: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: BorderSide(
+          color: isActive ? orange : gris,
+          width: 1,
+        ),
+      ),
+    ),
+    child: Text(
+      labelText,
+      style: TextStyle(
+        color: isActive ? orange : gris,
+        fontSize: 14,
+      ),
+    ),
+  );
+}
--- lib/src/features/presentation/filter_page/view/filter_page.dart
+++ lib/src/features/presentation/filter_page/view/filter_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ytb_flutter_delivery_firebase/src/colors/colors.dart';
 import 'package:ytb_flutter_delivery_firebase/src/features/presentation/commons_widgets/text_header.dart';
+import 'package:ytb_flutter_delivery_firebase/src/features/presentation/filter_page/custom_widgets/cuisines_filter.dart';
 
 class FilterPage extends StatefulWidget {
   FilterPage({Key? key}) : super(key: key);
@@ -14,8 +15,26 @@ class _FilterPageState extends State<FilterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(context),
-      body: Center(
-        child: Text('Filter'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(top: 15, bottom: 15, left: 15),
+              child: textHeader(
+                'CUISINES',
+                color: gris,
+                fontWeight: FontWeight.w600,
+                fontSize: 17,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: CuisinesFilter(),
+            ),
+          ]))
+        ],
       ),
     );
   }
@@ -25,9 +44,12 @@ PreferredSizeWidget _appBar(BuildContext context) {
   return AppBar(
     elevation: 0.5,
     backgroundColor: Colors.white,
-    title: textHeader(
-      'Filter',
-      fontWeight: FontWeight.w600,
+    title: Container(
+      alignment: Alignment.center,
+      child: textHeader(
+        'Filter',
+        fontWeight: FontWeight.w600,
+      ),
     ),
     leading: Container(
       padding: EdgeInsets.only(top: 20, left: 7),
