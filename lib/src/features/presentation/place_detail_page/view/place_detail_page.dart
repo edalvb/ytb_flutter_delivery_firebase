15dea419a6da040df3445b736b71cfdb97d38261
Edward Alexis <edalvb@gmail.com>
2022-01-20 22:56:25 -0500

    33 Diseño Place Detail Page Parte 2

--- lib/src/features/presentation/place_detail_page/view/place_detail_page.dart
+++ lib/src/features/presentation/place_detail_page/view/place_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ytb_flutter_delivery_firebase/src/colors/colors.dart';
+import 'package:ytb_flutter_delivery_firebase/src/features/presentation/commons_widgets/BackButtons/back_button.dart';
 import 'package:ytb_flutter_delivery_firebase/src/features/presentation/commons_widgets/Headers/text_header.dart';
 
 class PlaceDetailPage extends StatelessWidget {
@@ -32,35 +33,206 @@ class PlaceDetailPage extends StatelessWidget {
                   ),
                   Wrap(
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 121, left: 30, right: 15),
-                        width: double.infinity,
-                        height: 25,
-                        child: Row(
-                          children: [
-                            ElevatedButton(
-                              onPressed: () {},
-                              style: ElevatedButton.styleFrom(
-                                primary: orange,
-                                shape: StadiumBorder(),
-                              ),
-                              child: textHeader(
-                                'Free Delivery',
-                                color: Colors.white,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
+                      _promoBottom(),
+                      _infoPlace(),
+                      _infoPlaceStats(),
+                      _offerBanner(),
                     ],
                   )
                 ],
               ),
             ),
+            leading: Builder(builder: (BuildContext context) {
+              return backButton(context, Colors.white);
+            }),
+            actions: [
+              IconButton(
+                icon: const Icon(
+                  Icons.share,
+                  size: 28,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.bookmark_border,
+                  size: 28,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ],
           )
         ],
       ),
     );
   }
+}
+
+Widget _promoBottom() {
+  return Container(
+    margin: EdgeInsets.only(top: /*121,*/ 100, left: 30, right: 15),
+    width: double.infinity,
+    height: 25,
+    child: Row(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: orange,
+            shape: StadiumBorder(),
+          ),
+          child: textHeader(
+            'Free Delivery',
+            color: Colors.white,
+            fontSize: 12,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _infoPlace() {
+  return Column(
+    children: [
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        margin: EdgeInsets.symmetric(vertical: 20),
+        child: textHeader(
+          'Boon Lay Ho Huat Friend Prawn Noodle',
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        child: Row(
+          children: [
+            Icon(Icons.location_on, color: gris),
+            textHeader(
+              '03 Jameson Manors Apt. 177',
+              color: gris,
+              fontWeight: FontWeight.w500,
+              fontSize: 15,
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+Widget _infoPlaceStats() {
+  return Container(
+    margin: EdgeInsets.only(top: 20),
+    padding: EdgeInsets.symmetric(horizontal: 40),
+    height: 70,
+    decoration: const BoxDecoration(
+      border: Border(
+        top: BorderSide(color: Colors.white),
+        bottom: BorderSide(color: Colors.white),
+      ),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _tagInfoPlace(icon: Icons.star, text: '4.5', desc: '351 Ratings'),
+        _barSpacer(),
+        _tagInfoPlace(icon: Icons.bookmark, text: '137k', desc: 'Bookmak'),
+        _barSpacer(),
+        _tagInfoPlace(icon: Icons.photo, text: '346', desc: 'Photo'),
+      ],
+    ),
+  );
+}
+
+Widget _barSpacer() {
+  return Container(
+    height: 40,
+    decoration: const BoxDecoration(
+      border: Border(right: BorderSide(color: Colors.white)),
+    ),
+  );
+}
+
+Widget _tagInfoPlace({
+  required IconData icon,
+  required String text,
+  required String desc,
+}) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 19,
+          ),
+          textHeader(
+            text,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 15,
+          ),
+        ],
+      ),
+      textHeader(
+        desc,
+        color: gris,
+        fontWeight: FontWeight.w500,
+        fontSize: 15,
+      ),
+    ],
+  );
+}
+
+Widget _offerBanner() {
+  return Container(
+    color: const Color.fromRGBO(255, 237, 214, 1.0),
+    padding: EdgeInsets.all(20),
+    height: 90,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            textHeader(
+              'New! Try Pickup',
+              color: orange,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+            textHeader(
+              'Pickup on your time. Your order is \n ready when you are.',
+              color: primaryColor,
+              fontWeight: FontWeight.w400,
+              fontSize: 13,
+            ),
+          ],
+        ),
+        const Spacer(),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            primary: orange,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+          child: textHeader(
+            'Order Now',
+            color: Colors.white,
+            fontSize: 13,
+          ),
+        ),
+      ],
+    ),
+  );
 }
